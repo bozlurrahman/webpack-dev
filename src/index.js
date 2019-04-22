804fa641bbf203723afc6e4f88327fca0a0e84bc
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2019-04-22 19:51:23 +0600

    code cleaning for Output Management chapter

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ') + "<br>hey i am second line";
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  element.innerHTML += `<i class="icofont-box"></i>
-<i class="icofont-close-line" ></i>
-<i class="icofont-like"></i>
-<i class="icofont-plus"></i>
-<i class="icofont-home"></i>`;
-
-  console.log(Data);
 
   return element;
 }
